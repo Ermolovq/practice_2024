ce3b0a26f1de75e22001fb1b9c9963bb96151266
Ermolovq <145741855+Ermolovq@users.noreply.github.com>
2024-12-26 16:00:35 +0200

    update

--- footer.js
+++ footer.js
@@ -1,7 +1,8 @@
-document.getElementById("footer").innerHTML = `
-  <footer>
-    <div class="address">Наша адреса: <br>36039, м. Полтава, вул. Олеся Гончара, 25а</div>
-    <a href="login/" class="login">Для співробітників</a>
-    <div class="author">Сайт розробив<br> Єрмолов Вадим<br> студент 4 курсу<br>ВСП ППФК НТУ ХПІ</div>
-  </footer>
-`;
+const footer = document.createElement("footer")
+
+footer.innerHTML = 
+  `<div class="address">Наша адреса: <br>36039, м. Полтава, вул. Олеся Гончара, 25а</div>
+  <a href="login/" class="login">Для співробітників</a>
+  <div class="author">Сайт розробив<br> Єрмолов Вадим<br> студент 4 курсу<br>ВСП ППФК НТУ ХПІ</div>`
+
+ document.body.appendChild(footer)
