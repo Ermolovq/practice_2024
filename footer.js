4fa0c37b1c2d9ba9a081f665c45d1fd04761fca1
Ermolovq <145741855+Ermolovq@users.noreply.github.com>
2024-12-26 15:53:33 +0200

    Update footer.js

--- footer.js
+++ footer.js
@@ -1,6 +1,7 @@
-fetch("/footer.html")
-    .then(response => response.text())
-    .then(data => {
-      document.getElementById("footer").innerHTML = data;
-    })
-    .catch(error => console.error("Помилка завантаження футера:", error));
+document.getElementById("footer").innerHTML = `
+  <footer>
+    <div class="address">Наша адреса: <br>36039, м. Полтава, вул. Олеся Гончара, 25а</div>
+    <a href="login/" class="login">Для співробітників</a>
+    <div class="author">Сайт розробив<br> Єрмолов Вадим<br> студент 4 курсу<br>ВСП ППФК НТУ ХПІ</div>
+  </footer>
+`;
